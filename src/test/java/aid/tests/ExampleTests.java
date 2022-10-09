c7a4324ca312a0a4711edfcb0c1bd4ad0644c968
iwerrao <iwerrao@gmail.com>
2022-10-10 02:45:04 +0300

    3

--- src/test/java/aid/tests/ExampleTests.java
+++ src/test/java/aid/tests/ExampleTests.java
@@ -1,2 +1,13 @@
-package aid.tests;public class ExampleTests {
+package aid.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTests {
+    @Test
+    public void test1(){
+        Assert.assertTrue(true);
+        System.out.println();
+    }
+
 }
